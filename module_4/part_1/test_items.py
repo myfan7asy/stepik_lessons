209825a734e843981d03b0963913a6550b019f7c
Alexander Ramadanov <alexander.ramadanov@gmail.com>
2021-02-28 14:14:51 +0200

    Add to bag selector is changed and is not hard-coded anymore; test blocks are visually adjusted as per AAA principle

--- module_4/part_1/test_items.py
+++ module_4/part_1/test_items.py
@@ -1,10 +1,17 @@
 from helper import *
 from selenium.webdriver.common.by import By
 
+# Data block
 PRODUCT_URL = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+add_to_bag_locator = (By.CSS_SELECTOR, "button.btn-add-to-basket")
 
 
 def test_open_url_and_verify_button_text(driver, language):
+    # Arrange block
     driver.get(PRODUCT_URL)
-    add_to_cart_button = driver.find_element(By.CSS_SELECTOR, f"button[value='{locales[f'{language}']}']")
+
+    # Act block
+    add_to_cart_button = driver.find_element(*add_to_bag_locator)
+
+    # Assert block
     assert add_to_cart_button.text == locales[f"{language}"], "Button text is not as expected"
